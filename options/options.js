5f3ade82e8c301e2f26785529f0182723355d7da
Crowdbotics <team@crowdbotics.com>
2024-04-24 05:37:36 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soulstream-tv-47689",
+  url: "https://soulstream-tv-47689.botics.co",
+  api: "https://soulstream-tv-47689.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
